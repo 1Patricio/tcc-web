da98ea2978c3b966b46c1d13c71ac861abafb155
Anderson Patricio <andersongpatricio@hotmail.com>
2026-03-05 20:15:26 -0300

    fix: corrigindo problema de poder acessar rotas sem autenticação

--- src/router/index.ts
+++ src/router/index.ts
@@ -21,83 +21,90 @@ const router = createRouter({
       ]
     },
     {
-      path: '/app', 
+      path: '/app',
       component: () => import('@/layouts/DefaultLayout.vue'),
       children: [
         {
           path: '',
-          redirect: { name: 'home' }  
+          redirect: { name: 'home' },
         },
         {
           path: 'home',
           name: 'home',
           component: () => import('@/pages/HomePage.vue'),
-          meta: { requiresAuth: true }
+          meta: { requiresAuth: true },
         },
         {
           path: 'processos',
           name: 'processos',
           component: () => import('@/pages/processos/ProcessosPage.vue'),
-          meta: { requiresAuth: true }
+          meta: { requiresAuth: true },
         },
         {
           path: 'processos/:id',
           name: 'processo-view',
           component: () => import('@/pages/processos/ProcessoCadastroEdicao.vue'),
-          meta: { requiresAuth: true }
+          meta: { requiresAuth: true },
         },
         {
           path: 'processos/novo',
           name: 'processo-form',
           component: () => import('@/pages/processos/ProcessoCadastroEdicao.vue'),
-          meta: { requiresAuth: true }
+          meta: { requiresAuth: true },
         },
         {
           path: 'clientes',
           name: 'clientes',
           component: () => import('@/pages/clientes/ClientesPage.vue'),
-          meta: { requiresAuth: true }
+          meta: { requiresAuth: true },
         },
         {
           path: 'clientes',
           name: 'clientes',
           component: () => import('@/pages/clientes/ClientesPage.vue'),
-          meta: { requiresAuth: true }
+          meta: { requiresAuth: true },
         },
         {
           path: 'clientes/:id',
           name: 'cliente-view',
           component: () => import('@/pages/clientes/ClienteCadastroEdicao.vue'),
-          meta: { requiresAuth: true }
+          meta: { requiresAuth: true },
         },
         {
           path: 'clientes/novo',
           name: 'cliente-form',
           component: () => import('@/pages/clientes/ClienteCadastroEdicao.vue'),
-          meta: { requiresAuth: true }
+          meta: { requiresAuth: true },
         },
         {
-          path: 'documentos',
-          name: 'documentos',
-          component: () => import('@/pages/documentos/DocumentosPage.vue'),
-          meta: { requiresAuth: true }
+          path: 'pastas',
+          name: 'pastas',
+          component: () => import('@/pages/pastas/PastasPage.vue'),
+          meta: { requiresAuth: true },
         },
-      ]
+        {
+          path: 'pastas/:id',
+          name: 'arquivos',
+          component: () => import('@/pages/arquivos/ArquivosPage.vue'),
+          meta: { requiresAuth: true },
+        },
+      ],
     },
     {
-      path: '/:pathMatch(.*)*',  // rota 404
-      redirect: { name: 'login' }
-    }
+      path: '/:pathMatch(.*)*', // rota 404
+      redirect: { name: 'login' },
+    },
   ],
 })
 
 router.beforeEach((to, from) => {
   const token = localStorage.getItem('token')
-  const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+  const hasToken = Boolean(token && token !== 'null' && token !== 'undefined')
+  const requiresAuth = to.matched.some((record) => record.meta.requiresAuth)
 
-  if (requiresAuth && !token) {
+  if (requiresAuth && !hasToken) {
     return { name: 'login' }
-  } else if (!requiresAuth && token && to.name === 'login') {
+  } else if (!requiresAuth && hasToken && to.name === 'login') {
     return { name: 'home' }
   }
 
